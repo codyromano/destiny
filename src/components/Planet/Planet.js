973118b2f1679d589d5cbd56a08e25728b3d0a85
Cody Romano <codyromano@gmail.com>
2016-07-24 00:26:28 -0400

    Planet image preloading

--- src/components/Planet/Planet.js
+++ src/components/Planet/Planet.js
@@ -2,21 +2,36 @@ import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Planet.css';
 
+/* TODO: This belongs in a more general utility file */
+function preloadImage(img, preloadClass) {
+  // Mark image as preloading
+  img.classList.add(preloadClass);
+
+  let testImage = document.createElement('img');
+  testImage.addEventListener('load', () => {
+    // Remove preload indicator
+    img.classList.remove(preloadClass);
+  }, false);
+
+  testImage.src = img.src;
+}
+
 class Planet extends Component {
+  componentDidMount() {
+    let planetImage = this.refs.planetImage;
+    preloadImage(planetImage, s.preload);
+  }
   render() {
     let {name, imageSrc, width, discovered} = this.props;
-    width = width || 100;
-
     let style = {
       width: (width || 100) + '%'
     };
-
     let classList = [s.planetImage];
 
     return (<div ref="wrapper" className={s.PlanetWrapper}>
       <div className={s.planet}>
         <h2 className={s.planetHeading}>{name}</h2>
-        <img src={imageSrc} className={classList} style={style}/>
+        <img ref="planetImage" src={imageSrc} className={classList} style={style}/>
       </div>
     </div>);
   }
