d69082092be33c069c1f96e7d2298ff1ef949008
Cody Romano <codyromano@gmail.com>
2016-07-28 21:20:39 -0700

    makeNewComponent script

--- src/routes/explore/Explore.js
+++ src/routes/explore/Explore.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Explore.css';
+import Planet2 from '../../components/Planet2';
 
 const title = 'Explore';
 
@@ -11,6 +12,7 @@ function Explore(props, context) {
   return (
     <div className={s.root}>
       <div className={s.container}>
+        <Planet2/>
         <h1>{props.planetName}</h1>
         <p>...</p>
       </div>
