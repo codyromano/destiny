aa347b149e6f591b7027371dfef748ab81e501c7
Cody Romano <codyromano@gmail.com>
2016-08-03 18:51:57 -0700

    Fix geolocation direction bug

--- src/stores/DestinyStore.js
+++ src/stores/DestinyStore.js
@@ -9,6 +9,33 @@ let geo = {
 
 const CHANGE_EVENT = 'change';
 
+let objectives = [
+  {
+    id: 'Earth-1',
+    mainText: 'Go to the spa',
+    subText: 'Visit the spa with Amanda and Malissa.',
+    planet: 'Earth',
+    completed: false,
+    trackCoords: [47.617326, -122.330787]
+  },
+  {
+    id: 'Earth-2',
+    mainText: 'Go to the movies',
+    subText: 'Visit the movies with blah blah',
+    planet: 'Earth',
+    completed: false,
+    trackCoords: [47.617326, -122.330787]
+  },
+  {
+    id: 'Earth-3',
+    mainText: 'Go to the spa',
+    subText: 'Visit the spa with Amanda and Malissa.',
+    planet: 'Earth',
+    completed: false,
+    trackCoords: [47.617326, -122.330787]
+  }
+];
+
 let DestinyStore = Object.assign({}, EventEmitter.prototype, {
   getAll: function() {
     return Object.assign({}, geo);
@@ -47,42 +74,35 @@ let DestinyStore = Object.assign({}, EventEmitter.prototype, {
   },
 
   getObjectives: function() {
-    let objectives = [
-      {
-        id: 'Earth-1',
-        mainText: 'Go to the spa',
-        subText: 'Visit the spa with Amanda and Malissa.',
-        planet: 'Earth',
-        completed: false,
-        trackCoords: [47.617326, -122.330787]
-      },
-      {
-        id: 'Earth-2',
-        mainText: 'Go to the movies',
-        subText: 'Visit the movies with blah blah',
-        planet: 'Earth',
-        completed: false,
-        trackCoords: [47.617326, -122.330787]
-      },
-      {
-        id: 'Earth-3',
-        mainText: 'Go to the spa',
-        subText: 'Visit the spa with Amanda and Malissa.',
-        planet: 'Earth',
-        completed: false,
-        trackCoords: [47.617326, -122.330787]
-      }
-    ];
     return objectives;
   },
 
   getDirection: function(coordsA, coordsB) {
+    let latDescriptor = '',
+        lonDescriptor = '';
+
+    let latDiff = coordsA[0] - coordsB[0];
+    if (latDiff > 0) {
+      latDescriptor = 'east';
+    } else if (latDiff < 0) {
+      latDescriptor = 'west';
+    }
+
+    let lonDiff = coordsA[0] - coordsB[0];
+    if (lonDiff > 0) {
+      lonDescriptor = 'north';
+    } else if (lonDiff < 0) {
+      lonDescriptor = 'south';
+    }
+
+    if (!latDescriptor.length && !lonDescriptor.length) {
+      return 'here';
+    }
+
     // Only works for Northern Hemisphere...writing all this
     // in a hurry because it's a just for fun and I don't have a
     // lot of time...don't judge me.
-    let latOffset = (coordsA[0] < coordsB[0]) ? 'south' : 'south';
-    let lonOffset = (coordsA[1] < coordsB[1]) ? 'west' : 'east';
-    return [latOffset, lonOffset].join('');
+    return [lonDescriptor, latDescriptor].join('');
   },
 
   getDist: function(coordsA, coordsB) {
@@ -114,9 +134,18 @@ function getGeolocation() {
   }
   window.navigator.geolocation.watchPosition(function(pos) {
     geo.userCoords = pos.coords;
+    console.log(pos.coords);
     DestinyStore.emitChange();
   });
 }
+
+let spaceNeedle = [47.655899,-122.202289];
+let univDistrict = [47.660755,-122.328289];
+let bremerton = [47.558368,-122.643926];
+
+console.assert(DestinyStore.getDirection(spaceNeedle, univDistrict) === 'southwest', 'The spaceNeedle is southwest of the U-district.');
+
+console.assert(DestinyStore.getDirection(spaceNeedle, bremerton) === 'northeast', 'The spaceNeedle is northeast of bremerton.');
 
 AppDispatcher.register(function(action) {
   switch(action.actionType) {
