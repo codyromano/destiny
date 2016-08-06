88302a158752d13af362ce2e120bbe984751b0b6
Cody Romano <codyromano@gmail.com>
2016-08-05 21:25:36 -0700

    Travel route

---
+++ src/routes/travel/Travel.js
@@ -0,0 +1,54 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React, { PropTypes } from 'react';
+import CutScene from '../../components/CutScene';
+
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Travel.css';
+
+const title = 'Destiny';
+
+function Travel({ planetName }, context) {
+  //context.setTitle(title);
+  /*
+          <ul className={s.news}>
+          {news.map((item, index) => (
+            <li key={index} className={s.newsItem}>
+              <a href={item.link} className={s.newsTitle}>{item.title}</a>
+              <span
+                className={s.newsDesc}
+                dangerouslySetInnerHTML={{ __html: item.contentSnippet }}
+              />
+            </li>
+          ))}
+        </ul>
+  */
+  let planetURI = "/explore/" + planetName;
+
+  return (
+    <div className={s.root}>
+      <CutScene fadeOutSpeed="500" nextUrl={planetURI} videoSrc="visit-planet.mp4"/>
+      <div className={s.container}></div>
+    </div>
+  );
+}
+
+/*
+Travel.propTypes = {
+  news: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    contentSnippet: PropTypes.string,
+  })).isRequired,
+};
+Travel.contextTypes = { setTitle: PropTypes.func.isRequired };
+*/
+
+export default withStyles(s)(Travel);
