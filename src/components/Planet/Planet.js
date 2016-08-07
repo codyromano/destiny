fea31e22c5ad814b479de35e009d6c42b63dcf64
Cody Romano <codyromano@gmail.com>
2016-08-07 01:33:43 -0700

    Obscure undiscovered planets

--- src/components/Planet/Planet.js
+++ src/components/Planet/Planet.js
@@ -21,24 +21,38 @@ class Planet extends Component {
     let planetImage = this.refs.planetImage;
     preloadImage(planetImage, s.preload);
 
-
+    this.refs.wrapper.addEventListener('click', (ev) => {
+      let target = ev.target;
+      if (target.classList.contains(s.planet)) {
+      }
+    }, true);
   }
+  visitPlanet() {
+    let explorePlanetURI = ['/travel/', this.props.name, '/'].join('');
+    window.location.href = explorePlanetURI;
+  }
+
+  teasePlanet() {
+  }
+
   render() {
     let {name, imageSrc, width, discovered} = this.props;
+    name = discovered ? name : '?';
+
     let style = {
       width: (width || 100) + '%'
     };
     let classList = [discovered ? s.planetImageDiscovered :
        s.planetImage];
-    let explorePlanetURI = ['/travel/', name, '/'].join('');
+
+    let planetAction = (discovered ? this.visitPlanet :
+      this.teasePlanet).bind(this);
 
     return (<div ref="wrapper" className={s.planetMainWrapper}>
-      <a href={explorePlanetURI}>
-        <div className={s.planet}>
-          <h2 className={s.planetHeading}>{name}</h2>
-          <img ref="planetImage" src={imageSrc} className={classList} style={style}/>
-        </div>
-      </a>
+      <div className={s.planet} onClick={planetAction}>
+        <h2 className={s.planetHeading}>{name}</h2>
+        <img ref="planetImage" src={imageSrc} className={classList} style={style}/>
+      </div>
     </div>);
   }
 }
