36d157c8e276d3a583ff1e279a2d84ce9bf51623
Cody Romano <codyromano@gmail.com>
2016-08-10 16:56:26 -0700

    Mobile video support

--- src/components/CutScene/CutScene.js
+++ src/components/CutScene/CutScene.js
@@ -1,19 +1,54 @@
 import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './CutScene.css';
+ 
+// Not a great approach, but good enough for this
+ var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
 
 class CutScene extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playRequiresTouch: false,
+      playing: false
+    };
+  }
+
   componentDidMount() {
     let video = this.refs.cutSceneVideo;
     video.classList.add(s.playing);
 
-    this.refs.wrapper.addEventListener('click',
-      this.skipScene.bind(this), false);
+    if (isMobile.any()) {
+      this.addPlayOnTouchListener();
+      this.setState({
+        playRequiresTouch: true
+      });
+    }
   }
 
-  componentWillUnmount() {
-    this.refs.wrapper.removeEventListener('click',
-      this.skipScene.bind(this));
+  addPlayOnTouchListener() {
+    this.refs.cutSceneVideo.addEventListener('touchstart', (ev) => {
+      this.play();
+    });
   }
 
   skipScene() {
@@ -22,9 +57,12 @@ class CutScene extends Component {
 
   play() {
     let video = this.refs.cutSceneVideo;
-
     video.classList.add(s.playing);
     video.play();
+
+    this.setState({
+      playing: true
+    });
   }
 
   ended() {
@@ -36,12 +74,24 @@ class CutScene extends Component {
       window.location.href = this.props.nextUrl;
     }, fade);
   }
+
+  isPlaying() {
+    let video = this.refs.cutSceneVideo;
+    return (video.paused === false);
+  }
   
   render() {
     var src = '/videos/' + this.props.videoSrc;
+    let touchText = '';
+
+    if (this.state.playRequiresTouch && !this.state.playing) {
+      touchText = (<div className={s.touchText}>Tap to play cutscene</div>);
+    }
 
     return (<div ref="wrapper" className={s.cutSceneWrapper}>
-      <video onEnded={this.ended.bind(this)} 
+      {touchText}
+      <video 
+        onEnded={this.ended.bind(this)} 
         autoPlay="true"
         onPlay={this.play.bind(this)} ref="cutSceneVideo" 
         src={src} className={s.cutSceneVideo}/>
