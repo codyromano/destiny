f4cbad5ae16b11a0168041cfea2afa0ca04b2ff2
codyromano <codyromano@gmail.com>
2016-08-25 16:57:32 -0700

    Change default distance threshold

--- src/stores/DestinyStore.js
+++ src/stores/DestinyStore.js
@@ -167,7 +167,7 @@ function getThreshold() {
   // return localStore.get('threshold') || 0.05;
   let threshold = localStore.get('threshold');
   if (threshold === null || isNaN(threshold)) {
-    return 0.025;
+    return 0.01;
   }
   return parseFloat(threshold);
 }
